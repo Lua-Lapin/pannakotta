8a6c298325b4e3c250c6fc573aa3d4edaebc3dce
Code Drip Developer <developer@example.com>
2026-05-02 00:20:40 +0900

    refactor: LiverModal.jsxのgen情報をliver.genからpropに変更

--- src/components/LiverModal.jsx
+++ src/components/LiverModal.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from 'react'
 import { X, Diamond, Link } from 'lucide-react'
 import { GEN_META } from '../data/livers'
 
-export default function LiverModal({ liver, onClose }) {
+export default function LiverModal({ liver, gen, onClose }) {
   const closeRef = useRef(null)
 
   useEffect(() => {
@@ -21,7 +21,7 @@ export default function LiverModal({ liver, onClose }) {
   }, [onClose])
 
   const fanParts = [liver.fanMark, liver.fanName].filter(Boolean)
-  const genLabel = GEN_META[liver.gen]?.title ?? `${liver.gen}期生`
+  const genLabel = GEN_META[gen]?.title ?? `${gen}期生`
   const { twitter, reality, link } = liver.links ?? {}
 
   return (
